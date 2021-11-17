9657f5b7be26c6fd53977801e421f8a468ba9892
PhRezende-eng <ph.hr.001@gmail.com>
2021-11-16 23:25:57 -0300

    - fix

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ProductsOverviewPage(title: 'Flutter Demo Home Page'),
+      home: ProductsOverviewPage(title: 'Shop'),
       debugShowCheckedModeBanner: false,
     );
   }
