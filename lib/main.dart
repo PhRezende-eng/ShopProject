b4398d073bd9929bad37846a10fece823d4c1143
PhRezende-eng <ph.hr.001@gmail.com>
2021-11-30 07:47:28 -0300

    - minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/pages/product_datail_page.dart';
-import 'package:shop/pages/product_datail_provider.dart';
 import 'package:shop/pages/products_overview_page.dart';
-import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 
 void main() {
