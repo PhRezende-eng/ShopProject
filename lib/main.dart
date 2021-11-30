21e6dd9ee08914d88866307ba595cb8b46c10368
PhRezende-eng <ph.hr.001@gmail.com>
2021-11-29 23:51:32 -0300

    - minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_datail_page.dart';
 import 'package:shop/pages/product_datail_provider.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/providers/counter.dart';
@@ -13,25 +14,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        title: 'Shop',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.deepOrangeAccent,
-            background: Colors.orange[200],
-            surface: Colors.grey[100], //cores de strings
-          ),
-          fontFamily: 'Poppins',
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrangeAccent,
+          background: Colors.orange[200],
+          surface: Colors.grey[100], //cores de strings
         ),
-        home: const ProductsOverviewPage(title: 'Shop'),
-        routes: {
-          AppRooutes.PRODUCT_DETAIL: (context) => const ProductExampProvider(),
-        },
-        debugShowCheckedModeBanner: false,
+        fontFamily: 'Poppins',
       ),
+      home: const ProductsOverviewPage(title: 'Shop'),
+      routes: {
+        AppRooutes.PRODUCT_DETAIL: (context) => const ProductDetailPage(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
