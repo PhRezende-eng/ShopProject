86c4603b04a2ef76d6280da99bdb8941aec3bf5e
PhRezende-eng <ph.hr.001@gmail.com>
2021-12-13 12:00:49 -0300

    - Improvments for the logic

--- lib/logica.dart
+++ lib/logica.dart
@@ -2,20 +2,27 @@
 
 import 'dart:io';
 
-double calculaMedia(double nota1, double nota2, double notaOp) {
-  return ((nota1 + nota2 + notaOp) / 3);
+double calculaOp0(double notaOp) {
+  if (notaOp == 0) {
+    return -1;
+  }
+  return notaOp;
 }
 
 void calculaOptativa(double nota1, double nota2, double notaOp) {
   if (nota1 < nota2) {
-    nota1 = notaOp;
+    nota1 = nota1 > notaOp ? nota1 : notaOp;
   } else {
-    nota2 = notaOp;
+    nota2 = nota2 > notaOp ? nota2 : notaOp;
   }
 }
 
+double calculaMedia(double nota1, double nota2) {
+  return ((nota1 + nota2) / 2);
+}
+
 void calculaResultado(double nota1, double nota2, double notaOp) {
-  double media = calculaMedia(nota1, nota2, notaOp);
+  double media = calculaMedia(nota1, nota2) + notaOp * 0.2;
   if (media >= 6) {
     print('Aprovado');
   } else if (media < 3) {
@@ -23,13 +30,6 @@ void calculaResultado(double nota1, double nota2, double notaOp) {
   } else {
     print('Exame');
   }
-}
-
-double calculaOp0(double notaOp) {
-  if (notaOp == 0) {
-    return -1;
-  }
-  return notaOp;
 }
 
 main() {
