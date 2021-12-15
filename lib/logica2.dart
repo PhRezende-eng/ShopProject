da22bdd6e8f3611c75887af2dea126c904c358b2
PhRezende-eng <ph.hr.001@gmail.com>
2021-12-14 21:26:06 -0300

    Percorrendo um map para retornar as lojas que estao em produçaoo

---
+++ lib/logica2.dart
@@ -0,0 +1,340 @@
+Map flavors = {
+  "alecrimorganicos": {
+    "id": "br.com.alecrimorganicos",
+    "name": "Alecrim Organicos"
+  },
+  "armazemdogeraldo": {
+    "id": "br.com.armazemdogeraldo",
+    "name": "Armazem do Geraldo"
+  },
+  "atacadaohortifruti": {
+    "id": "br.com.atacadaohortifruti",
+    "name": "Atacadão Hortifruti"
+  },
+  "bemvindosupermercado": {
+    "id": "br.com.bemvindosupermercado",
+    "name": "Bem-Vindo Supermercados"
+  },
+  "bigbox": {"id": "br.com.bigbox", "name": "BIG BOX Delivery"},
+  "brilhodosol": {"id": "br.com.brilhodosol", "name": "Brilho do Sol"},
+  "campelo": {"id": "br.com.campelo", "name": "Campelo"},
+  "canteiros": {"id": "br.com.canteiros", "name": "Supermercado Canteiros"},
+  "cato": {"id": "br.com.cato", "name": "Cato Supermercados"},
+  "chocoart": {"id": "br.com.chocoart", "name": "Chocoart"},
+  "comercialreis": {
+    "id": "br.com.comercialreis",
+    "name": "Comercial Reis Supermercados"
+  },
+  "cornerexpress": {"id": "br.com.cornerexpress", "name": "Corner Express"},
+  "cultivida": {"id": "br.com.cultivida", "name": "Cultivida Brasil"},
+  "despensadobasilio": {
+    "id": "br.com.despensadobasilio",
+    "name": "Despensa do Basilio"
+  },
+  "emporiojuca": {
+    "id": "br.com.emporiojuca",
+    "name": "Juca - Empório e Mercearia"
+  },
+  "emporioqueijocompanhia": {
+    "id": "br.com.emporioqueijocompanhia",
+    "name": "Queijo e Companhia Delivery"
+  },
+  //  "engeluz": {
+  //  "id": "br.com.engeluz",
+  //  "name": "Engeluz Home Center"
+  //  },
+  "entregaki": {"id": "br.com.entregaki", "name": "Entregaki"},
+  "extrabom": {"id": "br.com.extrabom", "name": "Aniversario Extrabom"},
+  //  "favorito": {
+  //  "id": "br.com.superfavorito",
+  //  "name": "Supermercado Favorito"
+  //  },
+  "fazendamangalo": {
+    "id": "br.com.fazendamangalo",
+    "name": "Serra d´Água Delivery"
+  },
+  "fazendaorganica": {
+    "id": "br.com.fazendaorganica",
+    "name": "Fazenda Organica"
+  },
+  "feirasuperfacil": {
+    "id": "br.com.feirasuperfacil",
+    "name": "Feira Super Facil"
+  },
+  "fradheorganicos": {
+    "id": "br.com.fradheorganicos",
+    "name": "Fradhe Orgânicos"
+  },
+  "freguesia": {"id": "br.com.freguesia", "name": "Supermercado da Freguesia"},
+  "grafinosrestaurante": {
+    "id": "br.com.grafinosrestaurante",
+    "name": "Grã-Finos Restaurante"
+  },
+  "guarapari": {"id": "br.com.guarapari", "name": "Supermercado Guarapari"},
+  "hiperaraguaia": {"id": "br.com.hiperaraguaia", "name": "Hiper Araguaia"},
+  "hortimais": {"id": "br.com.hortimais", "name": "Rede Hortimais"},
+  "idalia": {"id": "br.com.idalia", "name": "Idalia Supermercado"},
+  "ifruit": {"id": "br.com.ifruit", "name": "iFruit"},
+  "indio": {"id": "br.com.supermercadosindio", "name": "Supermercados Indio"},
+  "industrial": {"id": "br.com.industrial", "name": "Supermercado Industrial"},
+  "instabuy": {"id": "br.com.instabuy", "name": "Instabuy"},
+  "inthehouse": {"id": "br.com.inthehouse", "name": "Mercadinho In The House"},
+  "korin": {"id": "br.com.korin", "name": "Korin"},
+  "korinmoema": {"id": "br.com.korinmoema", "name": "Korin Delivery SP"},
+  "lapalmaasasul": {"id": "br.com.lapalmaasasul", "name": "La Palma"},
+  "loveorganicos": {"id": "br.com.loveorganicos", "name": "Love Organicos"},
+  "melhoratacadista": {
+    "id": "br.com.melhoratacadista",
+    "name": "Melhor Atacadista"
+  },
+  "menegussi": {"id": "br.com.menegussi", "name": "Supermercado Menegussi"},
+  "meucantodaroca": {
+    "id": "br.com.meucantodaroca",
+    "name": "Meu Canto da Roca"
+  },
+  //  "meusuperaraujo": {
+  //  "id": "br.com.meusuperaraujo",
+  //  "name": "Supermercado Araújo"
+  //  },
+  "mixbahia": {"id": "br.com.mixbahia", "name": "Mix Bahia Supermercados"},
+  "mundial": {"id": "br.com.supermundial", "name": "Mundial Delivery"},
+  "negreiros": {"id": "br.com.negreiros", "name": "Supermercados Negreiros"},
+  "novomix": {"id": "br.com.novomix", "name": "Novomix"},
+  "opcao": {"id": "br.com.opcao", "name": "Super Opcao"},
+  "organicodochico": {
+    "id": "br.com.organicodochico",
+    "name": "Orgânico do Chico"
+  },
+  "paesevinhos": {
+    "id": "br.com.paesevinhos",
+    "name": "Supermercado Pães e Vinhos"
+  },
+  "pedralli": {"id": "br.com.pedralli", "name": "Supermercado Pedralli"},
+  "piante": {"id": "br.com.piante", "name": "Piante Hortifruti"},
+  "precobomplus": {"id": "br.com.precobomplus", "name": "Super Preco Bom Plus"},
+  "quitandasidro": {
+    "id": "br.com.quitandasidro",
+    "name": "Fazenda Quinta de Sidrô"
+  },
+  "quitandatomio": {"id": "br.com.quitandatomio", "name": "Quitanda Tomio"},
+  "regente": {"id": "br.com.regente", "name": "Supermercado Regente"},
+  "rotadecasa": {"id": "br.com.rotadecasa", "name": "Rota de casa"},
+  "sitiodubin": {"id": "br.com.sitiodubin", "name": "Sitio Dubin"},
+  "solovivo": {"id": "br.com.solovivo", "name": "Organicos Solo Vivo"},
+  "superbigbom": {"id": "br.com.superbigbom", "name": "Super BIGBOM"},
+  "superbombsb": {"id": "br.com.superbombsb", "name": "Superbom"},
+  "superbompreco": {"id": "br.com.superbompreco", "name": "Super Bom Preco"},
+  "supermercadobompreco": {
+    "id": "br.com.supermercadobompreco",
+    "name": "Bom Preco Delivery"
+  },
+  "supercaique": {"id": "br.com.supercaique", "name": "Caique Delivery"},
+  "superesteves": {"id": "br.com.superesteves", "name": "Super Esteves"},
+  "supermaisperto": {
+    "id": "br.com.supermaisperto",
+    "name": "Supermercado Mais Perto"
+  },
+  "supermercadoavenida": {
+    "id": "br.com.supermercadoavenida",
+    "name": "Super Avenida Delivery"
+  },
+  "supermercadobomvizinho": {
+    "id": "br.com.supermercadobomvizinho",
+    "name": "Supermercado Bom Vizinho"
+  },
+  "supermercadodiniz": {
+    "id": "br.com.supermercadodiniz",
+    "name": "Supermercado Diniz"
+  },
+  "supermercadolider": {
+    "id": "br.com.supermercadolider",
+    "name": "Supermercado Lider"
+  },
+  "supermercadorafaela": {
+    "id": "br.com.supermercadorafaela",
+    "name": "Supermercado Rafaela"
+  },
+  "supermercadosantana": {
+    "id": "br.com.supermercadosantana",
+    "name": "Supermercado Santana"
+  },
+  "supermercadovitoria": {
+    "id": "br.com.supermercadovitoria",
+    "name": "Supermercado Vitória"
+  },
+  "superolhodagua": {
+    "id": "br.com.superolhodagua",
+    "name": "Supermercado Olho D Agua"
+  },
+  "superpaodourado": {
+    "id": "br.com.superpaodourado",
+    "name": "Supermercado Pao Dourado"
+  },
+  "superpaomaster": {"id": "br.com.superpaomaster", "name": "Super Pão Master"},
+  "superprimavera": {"id": "br.com.superprimavera", "name": "Super Primavera"},
+  "superromao": {"id": "br.com.superromao", "name": "Super Romao"},
+  "superveneza": {"id": "br.com.superveneza", "name": "Super Veneza"},
+  "tatenosupermercado": {
+    "id": "br.com.tatenosupermercado",
+    "name": "Tateno Supemercado"
+  },
+  //  "tetra": {
+  //  "id": "br.com.tetra",
+  //  "name": "Tetra Solucoes"
+  //  },
+  "triunfo": {"id": "br.com.triunfo", "name": "Triunfo Supermercado"},
+  "vendinhadolago": {"id": "br.com.vendinhadolago", "name": "Vendinha do Lago"},
+  //  "vilabox": {
+  //  "id": "br.com.vilabox",
+  //  "name": "VilaBox Supermercado"
+  //  },
+  "vitaliaemporio": {"id": "br.com.vitaliaemporio", "name": "Vitalia Emporio"},
+  "webrewshop": {"id": "br.com.webrewshop", "name": "WE Brew Shop"},
+  "wesz": {"id": "br.com.wesz", "name": "Mercado e Açougue Wesz"},
+  "mikami": {"id": "br.com.mikami", "name": "Mikami"},
+  "super2000": {"id": "br.com.super2000", "name": "Super 2000"},
+  "ricardofrutas": {"id": "br.com.ricardofrutas", "name": "Ricardo Frutas"},
+  "seuverdurao": {"id": "br.com.seuverdurao", "name": "O Seu Verdurão"},
+  'mercadobrasilia': {
+    'id': 'br.com.mercadobrasilia',
+    'name': 'Mercado Brasília'
+  },
+  "happysupermarket": {
+    "id": "br.com.happysupermarket",
+    "name": "Happy Supermarket"
+  },
+  "supercouto40": {"id": "br.com.supercouto40", "name": "Super Couto 40"},
+  "supermercadopaonosso": {
+    "id": "br.com.supermercadopaonosso",
+    "name": "Supermercado Nosso Pão"
+  },
+  "minimercadomiscelania": {
+    "id": "br.com.minimercadomiscelania",
+    "name": "Mini Mercado Miscelânea"
+  },
+  "atacadao1000carnes": {
+    "id": "br.com.atacadao1000carnes",
+    "name": "Atacadão 1000 Carnes"
+  },
+  "bellavia": {"id": "br.com.bellavia", "name": "Bellavia Supermercados"},
+  "medeirossupermercados": {
+    "id": "br.com.medeirossupermercados",
+    "name": "Medeiros Supermercados"
+  },
+  "mercadobrumais": {"id": "br.com.mercadobrumais", "name": "Mercado Brumais"},
+  "ccomprac": {"id": "br.com.ccomprac", "name": "Compra Fácil Delivery"},
+  "barracadeorganico": {
+    "id": "br.com.barracadeorganico",
+    "name": "Barraca de Orgânicos"
+  },
+  "hortamar": {"id": "br.com.hortamar", "name": "Hortamar Delivery"},
+  "supermercadotiojoao": {
+    "id": "br.com.supermercadotiojoao",
+    "name": "Supermercado Tio João"
+  },
+  "biolojageranium": {
+    "id": "br.com.biolojageranium",
+    "name": "Bioloja Geranium"
+  },
+  "supermercadozeca": {
+    "id": "br.com.supermercadozeca",
+    "name": "Supermercado do Zeca"
+  },
+  "ugami": {"id": "br.com.ugami", "name": "Ugami Mercado Natural"},
+  "economiacerta": {
+    "id": "br.com.economiacerta",
+    "name": "Supermercado Economia Certa"
+  },
+  "atacadaomix": {"id": "br.com.atacadaomix", "name": "Mix Atacadão"},
+  "royallecosmeticos": {
+    "id": "br.com.royallecosmeticos",
+    "name": "Royalle Cosméticos"
+  },
+  "supermercadogg": {"id": "br.com.supermercadogg", "name": "Supermercado GG"},
+  "agoramesmo": {"id": "br.com.agoramesmo", "name": "Supermercado Agora Mesmo"},
+  "fortatacadista": {"id": "br.com.fortatacadista", "name": "Fort Atacadista"},
+  "supermercadocik": {
+    "id": "br.com.supermercadocik",
+    "name": "Supermercado CIK"
+  },
+  "mercadoorganico": {
+    "id": "br.com.mercadoorganico",
+    "name": "Mercado Orgânico"
+  },
+  "superjose": {"id": "br.com.superjose", "name": "Super José"},
+  "mareu": {"id": "br.com.mareu", "name": "Mercado Mareu"},
+  "sacolaodonadecasa": {
+    "id": "br.com.sacolaodonadecasa",
+    "name": "Sacolão Dona de Casa"
+  },
+  "mercadinhounicompras": {
+    "id": "br.com.mercadinhounicompras",
+    "name": "Mercadinho Unicompras"
+  },
+  "martmag": {"id": "br.com.martmag", "name": "Supermercados MartMag"},
+  "kuroda": {"id": "br.com.kuroda", "name": "Kuroda em casa"},
+  "maayway": {"id": "br.com.maayway", "name": "MaayWay"},
+  "cajarana": {"id": "br.com.cajarana", "name": "Mini Mercado Cajarana"},
+  "villaverdepicadinhos": {
+    "id": "br.com.villaverdepicadinhos",
+    "name": "Villa Verde Picadinhos"
+  },
+  "saodomingos": {
+    "id": "br.com.saodomingos",
+    "name": "Supermercado São Domingos"
+  },
+  "supermercadovictor": {
+    "id": "br.com.supermercadovictor",
+    "name": "Victor Supermercado"
+  },
+  "hortifrutiserrapilheira": {
+    "id": "br.com.hortifrutiserrapilheira",
+    "name": "Hortifruti Serrapilheira"
+  },
+  "shimodinhasupermercado": {
+    "id": "br.com.shimodinhasupermercado",
+    "name": "Shimodinha em casa"
+  },
+  "hiperunimar": {"id": "br.com.hiperunimar", "name": "Hiper Unimar"},
+  "compremais": {"id": "br.com.compremais", "name": "Compre Mais"},
+  "fazendavermelha": {
+    "id": "br.com.fazendavermelha",
+    "name": "Fazenda Vermelha Delivery"
+  },
+  "supermercadoschwalm": {
+    "id": "br.com.supermercadoschwalm",
+    "name": "Supermercado Schwalm"
+  },
+  "supernossacasa": {"id": "br.com.supernossacasa", "name": "Super Nossa Casa"},
+  "biobelladescartaveis": {
+    "id": "br.com.biobelladescartaveis",
+    "name": "Bio Bella Descartáveis"
+  },
+  "superluanova": {
+    "id": "br.com.superluanova",
+    "name": "Supermercado Lua Nova"
+  },
+  "glicia": {"id": "br.com.glicia", "name": "Glicia Express"},
+  "carrarolopes": {
+    "id": "br.com.carrarolopes",
+    "name": "Supermercado CarraroLopes"
+  },
+  "sayonaragourmet": {
+    "id": "br.com.sayonaragourmet",
+    "name": "Sayonara Gourmet"
+  },
+  "verdibairro": {
+    "id": "br.com.verdibairro",
+    "name": "Supermercado Verdí Bairro"
+  },
+  "nikoniko": {"id": "br.com.nikoniko", "name": "Niko Niko Mart"},
+  "yallahabibi": {"id": "br.com.yallahabibi", "name": "Yalla Habibi"},
+};
+
+main() {
+  String name = flavors.toString().replaceAll("}", ",}");
+
+  flavors.forEach((key, value) {
+    print(value['name']);
+  });
+}
