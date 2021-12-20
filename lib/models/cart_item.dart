b877dd84e0c36973a5fc811cff8fa1cc6cdde066
PhRezende-eng <ph.hr.001@gmail.com>
2021-12-19 21:20:15 -0300

    Create cart Modal

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,1 +1,15 @@
-class CartItem {}
+class CartItem {
+  final String id;
+  final String productId;
+  final String name;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    required this.id,
+    required this.productId,
+    required this.name,
+    required this.price,
+    required this.quantity,
+  });
+}
