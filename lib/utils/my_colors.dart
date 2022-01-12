d7701b5aa58bbf69652cef741a76ba52d8486b42
Paulo <ph.hr.001@gmail.com>
2022-01-12 11:11:39 -0300

    - add a example to put MY COLORS on my project

---
+++ lib/utils/my_colors.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static Color redColor = const Color(0xFFF10052); //just call the attribute
+}
+
+const Color redColor = Color(0xFFF10052); //just call the name const
