8d6736f1f6622ab60abc42edfa191331823e3022
Paulo <ph.hr.001@gmail.com>
2022-02-03 21:54:06 -0300

    - create the function to hide the keyBoard

--- lib/utils/util_functions.dart
+++ lib/utils/util_functions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 class Utils {
   static String formatPrice(double price) {
     var priceString = 'R\$ ${price.toStringAsFixed(2)}';
@@ -7,5 +9,9 @@ class Utils {
   static String formatPriceWithoutR$(double price) {
     var priceString = price.toStringAsFixed(2);
     return priceString.replaceFirst('.', ',');
+  }
+
+  static hideKeyBoard() {
+    FocusManager.instance.primaryFocus?.unfocus();
   }
 }
