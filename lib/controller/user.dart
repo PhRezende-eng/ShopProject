cb7299b04affd8e731900f84715b13dad9d31c23
Paulo <ph.hr.001@gmail.com>
2022-03-04 01:10:26 -0300

    mend

--- lib/controller/user.dart
+++ lib/controller/user.dart
@@ -2,38 +2,18 @@
 
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shop/models/user.dart';
 import 'package:shop/services/request_user.dart';
 
 class UserProvider with ChangeNotifier {
-  UserProvider();
+  UserModel? _user;
+  UserModel? get user => _user;
+  void _saveUser(UserModel? user) {
+    _user = user;
+  }
 
   List<UserModel> _usersLogin = [];
   List<UserModel> _usersRegister = [];
-
-  String? _email;
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-
-  UserModel? _user;
-  UserModel? get user => _user;
-  void setUser(UserModel? user) {
-    if (user != null) {
-      _user = user;
-    } else {
-      _user = null;
-    }
-  }
-
-  void setInMemoryUser(String email) async {
-    SharedPreferences prefs = await _prefs;
-    await prefs.setString('_user', email);
-  }
-
-  Future getInMemoryUser() async {
-    SharedPreferences prefs = await _prefs;
-    _email = prefs.getString('_user');
-  }
 
   Future getUsersFromDB(BuildContext context) async {
     await Provider.of<RequestUserProvider>(context, listen: false)
@@ -45,15 +25,6 @@ class UserProvider with ChangeNotifier {
         .getLoginUser()
         .then((usersLogin) => _usersLogin = usersLogin)
         .catchError((error) => _usersLogin = []);
-
-    await getInMemoryUser();
-    if (_email != null) {
-      setUser(
-        _usersLogin.firstWhere(
-          (element) => element.email == _email,
-        ),
-      );
-    }
   }
 
   bool canRegister(UserModel userRegister) {
@@ -76,7 +47,7 @@ class UserProvider with ChangeNotifier {
 
     if (!hasUserRegister && !hasUserLogin) {
       _usersLogin.insert(0, userLogin);
-      _user = userLogin;
+      _saveUser(userLogin);
       return true;
     } else {
       return false;
